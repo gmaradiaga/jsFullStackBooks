fd0dc38c3a86a32cf117301916ec49becd3a62a4
gmaradiaga <gonzalo.maradiaga.gmail.com>
2021-08-24 21:21:25 -0600

    Fourth Commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const devMode = process.env.NODE_ENV != 'production';
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: 'production',
